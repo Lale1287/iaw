790be868621578bc6e40d83344972cd947a71bf5
Lale1287 <alejandracolombini1287@gmail.com>
2022-10-11 01:43:49 -0300

    correcciones carga inicial

--- filehostingservice/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, FilehostingserviceApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<FilehostingserviceApplication>;
--- filehostingservice/dist/jobs/Myjob.d.ts
+++
@@ -1,6 +0,0 @@
-import { CronJob } from '@loopback/cron';
-import { FileRepository } from '../repositories/file.repository';
-export declare class MyCronJob extends CronJob {
-    fileRepository: FileRepository;
-    constructor(fileRepository: FileRepository);
-}
